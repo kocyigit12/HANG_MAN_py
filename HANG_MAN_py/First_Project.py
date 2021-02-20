796f57c1b22ae0cfebb6aaa917eaa3cce181ed29
yiğit koçyiğit <62246558+kocyigit12@users.noreply.github.com>
2021-02-20 17:11:23 +0300

    Update First_Project.py

--- HANG_MAN_py/First_Project.py
+++ HANG_MAN_py/First_Project.py
@@ -1,15 +1,8 @@
 
-# Step 1
+
 import random
-# word_list = ["aardvark", "baboon", "camel"]
 import hangman_words as word_list
 import hangman_art as art
-
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
 
 random_word =random.choice(word_list.word_list)
